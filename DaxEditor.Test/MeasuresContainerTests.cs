a10ea412cc349b7b8691bc780a2edf87c612a07b
Konstantin S. <havendv@gmail.com>
2016-12-31 02:26:18 +1000

    Excluded tests without a test data file.

--- DaxEditor.Test/MeasuresContainerTests.cs
+++ DaxEditor.Test/MeasuresContainerTests.cs
@@ -71,32 +71,7 @@ namespace DaxEditorSample.Test
                 StringAssert.Contains(e.Message, "KPI are not yet supported");
             }
         }
-
-        [TestMethod]
-        public void M3()
-        {
-            try
-            {
-                string input = Utils.ReadFileFromResources("M3.bim");
-                var bim = MeasuresContainer.ParseText(input);
-                Assert.IsNotNull(bim.Measures);
-                Assert.AreEqual(88, bim.Measures.Count);
-                var daxText = bim.GetDaxText();
-                Assert.IsNotNull(daxText);
-
-                var measuresFromDax = MeasuresContainer.ParseDaxScript(daxText);
-                Assert.AreEqual(bim.Measures.Count, measuresFromDax.Measures.Count);
-
-                var expected = input;
-                var actual = measuresFromDax.UpdateMeasures(input);
-                WindiffAssert.AreEqualNormalizedXmla(expected, actual);
-                Assert.Fail("Exception expected");
-            }
-            catch (Exception e)
-            {
-                StringAssert.Contains(e.Message, "KPI are not yet supported");
-            }
-        }
+        
 
         [TestMethod]
         public void Bim1100_Json()
@@ -146,63 +121,7 @@ namespace DaxEditorSample.Test
             var actual = measuresFromDax.UpdateMeasures(text);
             WindiffAssert.AreEqual(expected, actual);
         }
-
-        [TestMethod]
-        public void Storecard()
-        {
-            try
-            {
-                string input = Utils.ReadFileFromResources("Scorecard.bim");
-                var bim = MeasuresContainer.ParseText(input);
-                Assert.IsNotNull(bim.Measures);
-                //Assert.AreEqual(88, bim.Measures.Count);
-                var daxText = bim.GetDaxText();
-                Assert.IsNotNull(daxText);
-
-                var measuresFromDax = MeasuresContainer.ParseDaxScript(daxText);
-                Assert.AreEqual(bim.Measures.Count, measuresFromDax.Measures.Count);
-
-                var expected = input;
-                var actual = measuresFromDax.UpdateMeasures(input);
-                WindiffAssert.AreEqualNormalizedXmla(expected, actual);
-                Assert.Fail("Exception expected");
-            }
-            catch (Exception e)
-            {
-                StringAssert.Contains(e.Message, "KPI are not yet supported");
-            }
-        }
-
-        [TestMethod]
-        public void Storecard_Json()
-        {
-            var text = Utils.ReadFileFromResources("Scorecard_JSON.bim");
-            var bim = MeasuresContainer.ParseText(text);
-            Assert.IsNotNull(bim.Measures);
-            var daxText = bim.GetDaxText();
-            Assert.IsNotNull(daxText);
-
-            var measuresFromDax = MeasuresContainer.ParseDaxScript(daxText);
-            Assert.AreEqual(bim.Measures.Count, measuresFromDax.Measures.Count);
-
-            var expected = text;
-
-            //Fix sorting. But missing properties in the model will be hidden.
-            var document = JsonUtilities.Deserialize(text);
-            //kpi not supported
-            document?.Model?.Tables?.ForEach(table =>
-            {
-                table.Measures?.ForEach(measure =>
-                {
-                    measure.KPI = null;
-                });
-            });
-            expected = JsonUtilities.Serialize(document);
-
-            var actual = measuresFromDax.UpdateMeasures(text);
-            WindiffAssert.AreEqual(expected, actual);
-        }
-
+        
         [TestMethod]
         public void NewDaxModel_Json()
         {
