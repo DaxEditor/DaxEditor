4e68298f72628f0057bce8e59ca2b292aecdc6a5
marcorusso <marco.russo@loader.it>
2016-12-30 18:29:12 +0100

    Updated version and published to VS Gallery

--- DaxEditor.IntegrationTests/Properties/AssemblyInfo.cs
+++ DaxEditor.IntegrationTests/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.1611.1902")]
-[assembly: AssemblyFileVersion("2.0.1611.1902")]
+[assembly: AssemblyVersion("2.0.1612.3001")]
+[assembly: AssemblyFileVersion("2.0.1612.3001")]
--- DaxEditor/Properties/AssemblyInfo.cs
+++ DaxEditor/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.1611.1902")]
-[assembly: AssemblyFileVersion("2.0.1611.1902")]
+[assembly: AssemblyVersion("2.0.1612.3001")]
+[assembly: AssemblyFileVersion("2.0.1612.3001")]
 
 
