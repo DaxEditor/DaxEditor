060159feb636bea5696391d06e4fd4d9a2f38bda
marcorusso <marco.russo@loader.it>
2017-02-12 14:22:57 +0100

    Released build supporting measures starting with _ prefix

--- DaxEditor/Properties/AssemblyInfo.cs
+++ DaxEditor/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.1702.081")]
-[assembly: AssemblyFileVersion("2.0.1702.081")]
+[assembly: AssemblyVersion("2.0.1702.121")]
+[assembly: AssemblyFileVersion("2.0.1702.121")]
 
 
