89623565206abd04f5312f83c78e80e1b138bc51
Konstantin S. <havendv@gmail.com>
2017-02-15 15:59:18 +1000

    Removed unused namespaces 2

--- DaxEditor.Test/DaxFormatterComTests.cs
+++ DaxEditor.Test/DaxFormatterComTests.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using DaxEditor;
-
-namespace DaxEditorSample.Test
+﻿namespace DaxEditor.Test
 {
+    using System;
+    using NUnit.Framework;
+
     [TestFixture]
     public class DaxFormatterComTests
     {
