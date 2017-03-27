93d17963da27f6de5af5d1dd1465db4e1b3e90d8
Marco Russo <marco.russo@loader.it>
2017-03-26 18:22:05 -0700

    Updated version number

--- DaxEditor.IntegrationTests/Properties/AssemblyInfo.cs
+++ DaxEditor.IntegrationTests/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.1702.081")]
-[assembly: AssemblyFileVersion("2.0.1702.081")]
+[assembly: AssemblyVersion("2.0.1703.261")]
+[assembly: AssemblyFileVersion("2.0.1703.261")]
